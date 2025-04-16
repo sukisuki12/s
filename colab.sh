b31d66c5eba2bf39b7e8542a31c869356818ba90
sukisuki12 <sukisuki.kaito111@gmail.com>
2025-04-17 00:56:09 +0700

    Create colab.sh

---
+++ colab.sh
@@ -0,0 +1,51 @@
+wget -O ng.sh https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine/raw/main/ngrok.sh > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+
+function goto
+{
+    label=$1
+    cd 
+    cmd=$(sed -n "/^:[[:blank:]][[:blank:]]*${label}/{:a;n;p;ba};" $0 | 
+          grep -v ':$')
+    eval "$cmd"
+    exit
+}
+
+: ngrok
+clear
+echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok config add-authtoken $CRP 
+
+clear
+echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
+echo "======================="
+echo "choose ngrok region (for better connection)."
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRM
+./ngrok tcp --region $CRM 5900 &>/dev/null &
+sleep 1
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+docker pull rsolano/ubuntu-vnc
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+docker run --rm -p 5900:5900 --privileged --cap-add=SYS_PTRACE --shm-size=2g rsolano/ubuntu-vnc
+clear
+echo IP Address:
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
+echo User: ubuntu
+echo Passwd: ubuntu
+echo "VM can't connect? Restart Cloud Shell then Re-run script."
