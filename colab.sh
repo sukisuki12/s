63fac2a58776d64eb938e6709b65fe751b15beb0
sukisuki12 <sukisuki.kaito111@gmail.com>
2025-04-17 01:12:45 +0700

    Update colab.sh

--- colab.sh
+++ colab.sh
@@ -31,18 +31,18 @@ echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
 read -p "choose ngrok region: " CRM
-./ngrok tcp --region $CRM 5900 &>/dev/null &
+./ngrok tcp --region $CRM 5901 &>/dev/null &
 sleep 1
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
-docker pull rsolano/ubuntu-vnc
+docker pull thuonghai2711/ubuntu22-vnc-pulseaudio
 clear
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
 echo "===================================="
-docker run --rm -p 5900:5900 --privileged --cap-add=SYS_PTRACE --shm-size=2g rsolano/ubuntu-vnc
+docker run --rm --shm-size 1g --privileged thuonghai2711/ubuntu22-vnc-pulseaudio:xfce4
 clear
 echo IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' 
